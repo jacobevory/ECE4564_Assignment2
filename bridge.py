7afdb042c03cfe860ace3cdc86a1c5932a80320a
jacobevory <jacobevory@gmail.com>
2017-11-05 18:30:47 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+import os
+os.system("sudo bluetoothctl")
+os.system("connect F8:CF:C5:D1:4F:3E")
+
+serv = 127.000.000.001
+
+if len(sys.argv) > 1:
+    if (sys.argv[1] == '-s'):    serv = int(sys.argv[2])
+      
