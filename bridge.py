77ce97aa8efe735a3e10746b7ce6ffab95f68757
jacobevory <jacobevory@gmail.com>
2017-11-05 19:27:08 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
-import bluetooth
+import socket
 import os
 import sys
 
-serv = '127.0.0.1'
-btMAC = 'F8:CF:C5:D1:4F:3E'
-port = 1
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((btMAC, port))
+RMQserv = '127.0.0.1'
+MDBport = 27017
+RMQport = 5672
+btMAC = 'F8:CF:C5:D1:4F:3E' #MAC Address of Jacob's cellular device with bluetooth capability
+port = 3
+backlog = 1
+size = 1024
+
+try:
+    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    #s.connect((btMAC, port))
+    print('[Checkpoint 01] Created socket at 0.0.0.0 on port ', port)
+    s.bind((btMAC, port))
+    s.listen(backlog)
+    print('[Checkpoint 02] Listening for client connections')
+    client, address = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data)
+except socket.error as message:
+    if s:
+        s.close()
+    print("Could not open socket: " + str(message))
+    sys.exit(1)
+    
 while 1:
     input = raw_input()
     if input == "quit":
