6b9a91eca36f7657c6a0cb8bcf60c45922f76448
jacobevory <jmevory@vt.edu>
2017-11-05 21:46:59 -0500

    BTcom works!

--- bridge.py
+++ bridge.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import socket
+
+import bluetooth
 import os
 import sys
 
@@ -7,35 +8,28 @@ RMQserv = '127.0.0.1'
 MDBport = 27017
 RMQport = 5672
 btMAC = 'F8:CF:C5:D1:4F:3E' #MAC Address of Jacob's cellular device with bluetooth capability
-port = 3
+port = 1
 backlog = 1
 size = 1024
 
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+#s.connect((btMAC, port))
+print('[Checkpoint 01] Created socket at 0.0.0.0 on port ', port)
+s.bind(("", port))
+s.listen(backlog)
+print('[Checkpoint 02] Listening for client connections')
 try:
-    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    #s.connect((btMAC, port))
-    print('[Checkpoint 01] Created socket at 0.0.0.0 on port ', port)
-    s.bind((btMAC, port))
-    s.listen(backlog)
-    print('[Checkpoint 02] Listening for client connections')
     client, address = s.accept()
+    print('Accepted connection from ', address)
     while 1:
         data = client.recv(size)
         if data:
             print(data)
             client.send(data)
-except socket.error as message:
-    if s:
-        s.close()
-    print("Could not open socket: " + str(message))
-    sys.exit(1)
-    
-while 1:
-    input = raw_input()
-    if input == "quit":
-        break
-    s.send(input)
-sock.close()
+except:
+    print("Error occured, closing Socket")
+    client.close()
+    s.close()
 
 if len(sys.argv) > 1:
     if (sys.argv[1] == '-s'):    serv = int(sys.argv[2])
