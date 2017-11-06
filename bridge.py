c2a62fda3f352c77fd5749180096a40b11dd929a
jacobevory <jmevory@vt.edu>
2017-11-05 19:13:14 -0500

    bt1

--- bridge.py
+++ bridge.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
+import bluetooth
 import os
-os.system("sudo bluetoothctl")
-os.system("connect F8:CF:C5:D1:4F:3E")
+import sys
 
-serv = 127.000.000.001
+serv = '127.0.0.1'
+btMAC = 'F8:CF:C5:D1:4F:3E'
+port = 1
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.connect((btMAC, port))
+while 1:
+    input = raw_input()
+    if input == "quit":
+        break
+    s.send(input)
+sock.close()
 
 if len(sys.argv) > 1:
     if (sys.argv[1] == '-s'):    serv = int(sys.argv[2])
-      
+    
